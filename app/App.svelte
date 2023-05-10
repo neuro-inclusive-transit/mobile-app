297ac8cdcb9c92dfc01e1df72d29a016ff5dc8e1
katrinhaa <katrin.hartz14@gmail.com>
2023-05-10 11:12:40 +0200

    Button Componente eingefügt... App stürzt leider ab

--- app/App.svelte
+++ app/App.svelte
@@ -1,7 +1,17 @@
-<frame>
-    <Home />
-</frame>
+
 
 <script lang="ts">
-    import Home from './components/Home.svelte'
+    import Home from './components/Home.svelte';
+    import Button from './shared/Button.svelte';
 </script>
+
+<frame>
+
+    <div>
+        <h1>Hi Katrin</h1>
+        <p>Willkommen in der App! Klick den Button zum starten</p>
+    </div>
+    <Button>
+        Hier Klicken
+    </Button>
+</frame>
---
+++ app/components/shared/Button.svelte
@@ -0,0 +1,40 @@
+<script>
+    import { xlink_attr } from "svelte/internal";
+
+    export let type = 'primary';
+    export let flat = false;
+
+</script>
+
+<button class={type} class:flat={flat}>
+    <slot></slot>
+</button>
+
+<style>
+    button{
+        border: 0;
+        cursor: pointer;
+        border-radius: 100%;
+        padding: 16px 12px;
+        font-weight: bold;
+        box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    }
+
+    .primary{
+        background-color:#3B5BDB;
+        color: white;
+
+    }
+
+    .secondary{
+        background-color: white;
+        color: #3B5BDB;
+        border: 2px solid #3B5BDB;
+
+    }
+
+    .flat{
+        box-shadow: none;
+    }
+
+</style>
