35bde909a9fc5dd920653fad970fd5b4ec480b14
Finn Nils Gedrath <finn.gedrath@gmail.com>
2023-05-15 18:46:48 +0200

    fix: put button inside of a page

--- app/App.svelte
+++ app/App.svelte
@@ -2,16 +2,8 @@
 
 <script lang="ts">
     import Home from './components/Home.svelte';
-    import Button from './shared/Button.svelte';
 </script>
 
 <frame>
-
-    <div>
-        <h1>Hi Katrin</h1>
-        <p>Willkommen in der App! Klick den Button zum starten</p>
-    </div>
-    <Button>
-        Hier Klicken
-    </Button>
+    <Home />
 </frame>
--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -1,27 +1,31 @@
-<page>
-    <actionBar title="Home" />
-    <gridLayout>
-        <label class="info">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
-    </gridLayout>
-</page>
-
 <script lang="ts">
-    let message: string = "Blank Svelte Native App"
+  import Button from "@/components/shared/Button.svelte";
+
+  let message: string = "Blank Svelte Native App";
 </script>
 
-<style>
-    .info .fas {
-        color: #3A53FF;
-    }
+<page>
+  <actionBar title="Home" />
+  <stackLayout>
+    <label class="info">
+      <formattedString>
+        <span class="fas" text="&#xf135;" />
+        <span text=" {message}" />
+      </formattedString>
+    </label>
 
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
+    <Button>Hier Klicken</Button>
+  </stackLayout>
+</page>
+
+<style>
+  .info .fas {
+    color: #3a53ff;
+  }
+
+  .info {
+    font-size: 20;
+    horizontal-align: center;
+    vertical-align: center;
+  }
 </style>
