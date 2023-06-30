4bef0255a9f0db642270a6951d872676fab21fcc
Finn Nils Gedrath <finn.gedrath@gmail.com>
2023-06-30 15:42:07 +0200

    add first version of accordion

---
+++ app/shared/components/Accordion.svelte
@@ -0,0 +1,33 @@
+<script type="ts">
+  export let open = false;
+  export let customClass = "";
+
+  function toggle() {
+    open = !open;
+  }
+</script>
+
+<stackLayout class="wrapper {customClass}">
+  <gridLayout columns="*,auto" rows="auto" on:tap="{toggle}">
+    <slot name="header" />
+    <button text={open ? 'expand_less' : 'expand_more'} class="icon fs-m" row={0} col={1} on:tap="{toggle}" />
+  </gridLayout>
+
+  {#if open}
+  <stackLayout>
+    <slot name="content">
+      <label text="No content" />
+    </slot>
+  </stackLayout>
+  {/if}
+
+</stackLayout>
+
+<style>
+  .wrapper {
+    background-color: var(--color-background-dark);
+    border-radius: 5px;
+    padding: var(--s);
+  }
+</style>
+
--- app/views/liveView/RouteOverview.svelte
+++ app/views/liveView/RouteOverview.svelte
@@ -3,6 +3,7 @@
   import { journeys } from "~/stores";
   import { liveJourney } from "~/stores/liveJourney";
 
+  import Accordion from "~/shared/components/Accordion.svelte";
   import Contacts from "./Contacts.svelte";
   import RouteOverview from "./RouteOverview.svelte";
 
@@ -18,7 +19,10 @@
     <scrollView row={1} col={0}>
       <stackLayout>
         {#each $liveJourney?.sections ?? [] as section}
-          <label text="{section.transport.mode}" />
+        <Accordion customClass="m-b-l">
+          <label text="{section.transport.mode}" slot="header" />
+          <label text="Test Content" slot="content" />
+        </Accordion>
         {/each}
       </stackLayout>
     </scrollView>
